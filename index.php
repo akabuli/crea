2f4562f0edff7ea1df49d42689591b30f65fa94d
Ayat Haidari <ayatullahhaidari1@gmail.com>
2024-02-25 16:54:19 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * Main template file.
+ * 
+ * @package Crea
+ */
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CREA</title>
+</head>
+<body>
+    
+</body>
+</html>
